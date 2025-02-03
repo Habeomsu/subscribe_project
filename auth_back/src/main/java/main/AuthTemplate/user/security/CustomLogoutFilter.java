cfaff372d4a09fed5d62ec99ec788f90cb3986d1
Habeomsu <hbs5186@naver.com>
2025-02-03 13:17:22 +0900

    쿠키키 null에러 수정

--- auth_back/src/main/java/main/AuthTemplate/user/security/CustomLogoutFilter.java
+++ auth_back/src/main/java/main/AuthTemplate/user/security/CustomLogoutFilter.java
@@ -29,11 +29,13 @@ public class CustomLogoutFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
         doFilter((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse, filterChain);
     }
 
-    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
 
         // 경로가 logout , POST 방식인지 확인
         String requestUri = request.getRequestURI();
@@ -53,41 +55,48 @@ public class CustomLogoutFilter extends GenericFilterBean {
         // 쿠키에서 refresh 토큰 가져오기
         String refresh = null;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
 
-            if (cookie.getName().equals("refresh")) {
-
-                refresh = cookie.getValue();
+        // 쿠키가 null인지 확인
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refresh")) {
+                    refresh = cookie.getValue();
+                    break; // 쿠키를 찾으면 루프 종료
+                }
             }
         }
 
-        //refresh null check
+        // refresh null check
         if (refresh == null) {
 
             // ApiResult 생성
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._NOTFOUND_REFRESH_TOKEN.getCode(),ErrorStatus._NOTFOUND_REFRESH_TOKEN.getMessage(), null);
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._NOTFOUND_REFRESH_TOKEN.getCode(),
+                    ErrorStatus._NOTFOUND_REFRESH_TOKEN.getMessage(), null);
 
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
         }
 
-        //expired check
+        // expired check
         try {
             jwtUtil.isExpired(refresh);
         } catch (ExpiredJwtException e) {
 
             // ApiResult 생성
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._EXFIRED_REFRESH_TOKEN.getCode(),ErrorStatus._EXFIRED_REFRESH_TOKEN.getMessage(), null);
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._EXFIRED_REFRESH_TOKEN.getCode(),
+                    ErrorStatus._EXFIRED_REFRESH_TOKEN.getMessage(), null);
 
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
-        }catch (SignatureException e) { // 서명 오류 처리
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._REFRESH_TOKEN_SIGNATURE_ERROR.getCode(), "유효하지 않은 REFRESH JWT 서명입니다.", null);
+        } catch (SignatureException e) { // 서명 오류 처리
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._REFRESH_TOKEN_SIGNATURE_ERROR.getCode(),
+                    "유효하지 않은 REFRESH JWT 서명입니다.", null);
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
         } catch (JwtException e) {
             // 기타 JWT 관련 오류 처리
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._INVALID_REFRESH_TOKEN.getCode(), "유효하지 않은 REFRESH JWT입니다.", null);
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._INVALID_REFRESH_TOKEN.getCode(),
+                    "유효하지 않은 REFRESH JWT입니다.", null);
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
         }
@@ -97,29 +106,31 @@ public class CustomLogoutFilter extends GenericFilterBean {
         if (!category.equals("refresh")) {
 
             // ApiResult 생성
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._INVALID_REFRESH_TOKEN.getCode(),ErrorStatus._INVALID_REFRESH_TOKEN.getMessage(), null);
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._INVALID_REFRESH_TOKEN.getCode(),
+                    ErrorStatus._INVALID_REFRESH_TOKEN.getMessage(), null);
 
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
         }
 
-        //DB에 저장되어 있는지 확인
+        // DB에 저장되어 있는지 확인
         Boolean isExist = refreshRepository.existsByRefresh(refresh);
         if (!isExist) {
 
             // ApiResult 생성
-            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._NOFOUND_REFRESH_TOKEN.getCode(),ErrorStatus._NOFOUND_REFRESH_TOKEN.getMessage(), null);
+            ApiResult<?> apiResult = ApiResult.onFailure(ErrorStatus._NOFOUND_REFRESH_TOKEN.getCode(),
+                    ErrorStatus._NOFOUND_REFRESH_TOKEN.getMessage(), null);
 
             JsonResponseUtil.sendJsonResponse(response, HttpServletResponse.SC_UNAUTHORIZED, apiResult);
             return;
         }
         String username = jwtUtil.getUsername(refresh);
 
-        //로그아웃 진행//Refresh 토큰 DB에서 제거
-//        refreshRepository.deleteByRefresh(refresh);
+        // 로그아웃 진행//Refresh 토큰 DB에서 제거
+        // refreshRepository.deleteByRefresh(refresh);
         refreshRepository.deleteByUsername(username);
 
-        //Refresh 토큰 Cookie 값 0
+        // Refresh 토큰 Cookie 값 0
         Cookie cookie = new Cookie("refresh", null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
